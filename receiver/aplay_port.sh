dab282ac01499ec248d959489330cf3ada8fb79b
William Yang <williamyang2017@gmail.com>
2022-08-14 23:05:16 +1000

    Added proper prefix for git bash cli

--- receiver/aplay_port.sh
+++ receiver/aplay_port.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # aplay equivalent on windows which uses vlc to pipe in the raw audio data
 # change the raw sampling rate if it is specified differently
 # ./vlc.sh --demux=rawaud --rawaud-channels 1 --rawaud-samplerate 5000 --rawaud-fourcc s16l -
