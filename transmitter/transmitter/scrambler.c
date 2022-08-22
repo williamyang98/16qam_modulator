702997ce798e5cc768fcd0905354506a29622c8f
William Yang <williamyang2017@gmail.com>
2022-08-22 18:11:41 +1000

    Scrambler performs 8bit update simulataneously

--- transmitter/transmitter/scrambler.c
+++ transmitter/transmitter/scrambler.c
@@ -10,15 +10,6 @@
 uint16_t SCRAMBLER_SYNCWORD = 0;
 uint16_t SCRAMBLER_REG = 0;
 
-uint8_t scrambler_shift_reg() {
-	SCRAMBLER_REG = SCRAMBLER_REG << 1;
-	uint8_t v = 0;
-	v ^= ((SCRAMBLER_REG >> 14) & 0x1);
-	v ^= ((SCRAMBLER_REG >> 15) & 0x1);
-	SCRAMBLER_REG |= v;
-	return v;
-}
-
 void scrambler_reset() {
 	SCRAMBLER_REG = SCRAMBLER_SYNCWORD;
 }
@@ -28,10 +19,9 @@ void scrambler_init(uint16_t syncword) {
 	SCRAMBLER_SYNCWORD = syncword;
 }
 
+// NOTE: The scrambler is of type [14,15]
 uint8_t scrambler_process(uint8_t x) {
-	uint8_t y = 0;
-	for (int i = 7; i >= 0; i--) {
-		y |= scrambler_shift_reg() << i;
-	}
-	return x ^ y;
+	uint8_t mask = (SCRAMBLER_REG ^ (SCRAMBLER_REG << 1)) >> 8;
+	SCRAMBLER_REG = (SCRAMBLER_REG << 8) | mask;
+	return x ^ mask;
 }
