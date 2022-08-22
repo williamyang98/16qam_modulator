165c927f11e74fdf181c2b456b71fd755902554b
William Yang <williamyang2017@gmail.com>
2022-08-22 18:11:21 +1000

    Added 7bit lookup table for conv encoder

--- transmitter/transmitter/conv_enc.c
+++ transmitter/transmitter/conv_enc.c
@@ -8,10 +8,13 @@
 #include "conv_enc.h"
 
 
-const uint16_t CONV_ENC_REG_MASK  = 0b11100000000;
+const uint8_t CONV_ENC_REG_MASK  = 0b00111111;
 const uint8_t CONV_ENC_POLY_MASK  = 0b111;
 
-uint8_t CONV_ENC_TABLE[8] = {0};
+// our fast lookup table will have 6 bit lookup
+// 2 bits = reg state 1:2 | 4 bits = input bits
+// output of look up is the 8 bit encoded output
+uint8_t CONV_ENC_TABLE[64] = {0};
 uint8_t XOR_COUNT_LUT[8] = {0,1,1,0, 1,0,0,1};
 uint16_t CONV_ENC_REG = 0;
 
@@ -19,10 +22,18 @@ void conv_enc_calculate_table(const uint8_t G1, const uint8_t G2) {
 	uint8_t g1 = G1 & CONV_ENC_POLY_MASK;
 	uint8_t g2 = G2 & CONV_ENC_POLY_MASK;
 	
-	for (uint8_t i = 0; i < 8; i++) {
+	for (uint8_t i = 0; i < 0b01000000; i++) {
 		uint8_t y = 0;
-		y |= XOR_COUNT_LUT[(i & g1) & CONV_ENC_POLY_MASK] << 1;
-		y |= XOR_COUNT_LUT[(i & g2) & CONV_ENC_POLY_MASK] << 0;
+		uint8_t x = i;
+		
+		for (int i = 0; i < 4; i++) {
+			y = y << 2;
+			x = x << 1;
+			
+			uint8_t reg = (x & 0b01110000) >> 4;
+			y |= XOR_COUNT_LUT[(reg & g1) & CONV_ENC_POLY_MASK] << 1;
+			y |= XOR_COUNT_LUT[(reg & g2) & CONV_ENC_POLY_MASK] << 0;
+		}
 		
 		CONV_ENC_TABLE[i] = y;
 	}
@@ -36,30 +47,10 @@ int conv_enc_process(uint8_t* x, uint8_t* y, const int N) {
 	int k = 0;
 	for (int i = 0; i < N; i++) {
 		CONV_ENC_REG |= x[i];
+		y[k]   = CONV_ENC_TABLE[(CONV_ENC_REG >> 4) & CONV_ENC_REG_MASK];
+		y[k+1] = CONV_ENC_TABLE[CONV_ENC_REG & CONV_ENC_REG_MASK];
 		
-		//y[k] = x[i];
-		//y[k+1] = x[i];
-		
-		y[k]   = 0;
-		y[k+1] = 0;
-		
-		CONV_ENC_REG = CONV_ENC_REG << 1;
-		y[k] |= CONV_ENC_TABLE[(CONV_ENC_REG & CONV_ENC_REG_MASK) >> 8] << 6;
-		CONV_ENC_REG = CONV_ENC_REG << 1;
-		y[k] |= CONV_ENC_TABLE[(CONV_ENC_REG & CONV_ENC_REG_MASK) >> 8] << 4;
-		CONV_ENC_REG = CONV_ENC_REG << 1;
-		y[k] |= CONV_ENC_TABLE[(CONV_ENC_REG & CONV_ENC_REG_MASK) >> 8] << 2;
-		CONV_ENC_REG = CONV_ENC_REG << 1;
-		y[k] |= CONV_ENC_TABLE[(CONV_ENC_REG & CONV_ENC_REG_MASK) >> 8];
-		
-		CONV_ENC_REG = CONV_ENC_REG << 1;
-		y[k+1] |= CONV_ENC_TABLE[(CONV_ENC_REG & CONV_ENC_REG_MASK) >> 8] << 6;
-		CONV_ENC_REG = CONV_ENC_REG << 1;
-		y[k+1] |= CONV_ENC_TABLE[(CONV_ENC_REG & CONV_ENC_REG_MASK) >> 8] << 4;
-		CONV_ENC_REG = CONV_ENC_REG << 1;
-		y[k+1] |= CONV_ENC_TABLE[(CONV_ENC_REG & CONV_ENC_REG_MASK) >> 8] << 2;
-		CONV_ENC_REG = CONV_ENC_REG << 1;
-		y[k+1] |= CONV_ENC_TABLE[(CONV_ENC_REG & CONV_ENC_REG_MASK) >> 8];
+		CONV_ENC_REG = CONV_ENC_REG << 8;
 		
 		k += 2;
 	}
